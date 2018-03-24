4e48d73d6b31810f835f6a0b033a3a0b7d64d8dd
flo <flo@cis.lmu.de>
2018-03-24 18:17:08 +0100

    reimplementation of char map

--- map.go
+++ map.go
@@ -1,47 +1,110 @@
 package corpus
 
-// CharTriple represents a sequece of
-// three characters.
-type CharTriple [3]rune
+// Char3Grams is a map character triple counts
+type Char3Grams struct {
+	n uint64
+	m map[string]uint64
+}
 
-// CharTripleMap is a map character triple counts
-type CharTripleMap map[CharTriple]uint64
-
-// Add adds the triples of another map into a map.
-func (m CharTripleMap) Add(o CharTripleMap) CharTripleMap {
-	for k, v := range o {
-		m[k] += v
+// NewChar3Grams create a new Char3Grams instance.
+func NewChar3Grams() *Char3Grams {
+	return &Char3Grams{
+		m: make(map[string]uint64),
 	}
-	return m
+}
+
+// AddAll adds all character 3-grams of the
+// supplied string into the map.
+func (m *Char3Grams) AddAll(str string) {
+	EachChar3Gram(str, func(str string) {
+		m.m[str]++
+		m.n++
+	})
+}
+
+// Add3Grams adds the 3-grams of anohter map to this.
+func (m *Char3Grams) Add3Grams(o *Char3Grams) {
+	for k, v := range o.m {
+		m.m[k] += v
+	}
+	m.n += o.n
+}
+
+// Get returns the number of the supplied 3-gram.
+func (m *Char3Grams) Get(str string) uint64 {
+	c, ok := m.m[str]
+	if !ok {
+		return 0
+	}
+	return c
+}
+
+// Total returns the total number of 3-grams in the map.
+func (m *Char3Grams) Total() uint64 {
+	return m.n
+}
+
+// Len return the number of different 3-grams in the map.
+func (m *Char3Grams) Len() uint64 {
+	return uint64(len(m.m))
+}
+
+// // Add adds the triples of another map into a map.
+// func (m CharTripleMap) Add(o CharTripleMap) CharTripleMap {
+// 	for k, v := range o {
+// 		m[k] += v
+// 	}
+// 	return m
+// }
+
+// EachChar3Gram iterates of all character 3-grams in the given string.
+// It calls the supplied callback function for each such 3-gram.
+func EachChar3Gram(str string, f func(string)) {
+	pos := make([]int, 0, len(str)+1)
+	for i := range str {
+		pos = append(pos, i)
+		add3Gram(pos, str, f)
+	}
+	pos = append(pos, len(str))
+	add3Gram(pos, str, f)
+}
+
+func add3Gram(pos []int, str string, f func(string)) {
+	if len(pos) < 4 {
+		return
+	}
+	s := pos[len(pos)-4]
+	e := pos[len(pos)-1]
+	f(str[s:e])
 }
 
 // ReadCharTripleMap reads a new CharTripleMap from a token stream.
 // The second argument is the type of tokens that should be used.
 // To allow multiple types of tokens use | to combine them:
 // ReadCharTripleMap(t, Word | Number)
-func ReadCharTripleMap(t Tokener, tts TokenType) (CharTripleMap, error) {
-	m := make(CharTripleMap)
-	for token := range t.Tokens() {
-		if (tts & token.Type()) == 0 {
-			continue
-		}
-		var rs []rune
-		for _, r := range token.Str {
-			rs = shiftRune(rs, r)
-			if len(rs) == 3 {
-				m[CharTriple{rs[0], rs[1], rs[2]}]++
-			}
-		}
-	}
-	return m, t.Err()
-}
+// func ReadCharTripleMap(t Tokener, tts TokenType) (CharTripleMap, error) {
+// 	m := make(CharTripleMap)
+// 	for token := range t.Tokens() {
+// 		if (tts & token.Type()) == 0 {
+// 			continue
+// 		}
+// 		var rs []rune
+// 		for _, r := range token.Str {
+// 			rs = shiftRune(rs, r)
+// 			if len(rs) == 3 {
+// 				m[CharTriple{rs[0], rs[1], rs[2]}]++
+// 			}
+// 		}
+// 	}
+// 	return m, t.Err()
+// }
 
-func shiftRune(rs []rune, r rune) []rune {
-	if len(rs) < 3 {
-		return append(rs, r)
-	}
-	rs[0] = rs[1]
-	rs[1] = rs[2]
-	rs[2] = r
-	return rs
-}
+// func shiftRune(rs []rune, r rune) []rune {
+// 	if len(rs) < 3 {
+// 		return append(rs, r)
+// 	}
+// 	rs[0] = rs[1]
+// 	rs[1] = rs[2]
+// 	rs[2] = r
+// 	return rs
+// }
--- map_test.go
+++ map_test.go
@@ -2,68 +2,78 @@ package corpus
 
 import (
 	"fmt"
-	"strings"
+	"reflect"
 	"testing"
 )
 
-func TestReadCharTripleMap(t *testing.T) {
+func TestEachChar3Gram(t *testing.T) {
 	tests := []struct {
-		tokens string
-		ts     CharTriple
-		want   uint64
+		test string
+		want []string
 	}{
-		{"abc", CharTriple{'a', 'b', 'c'}, 1},
-		{"abc abc", CharTriple{'a', 'b', 'c'}, 2},
-		{"abcde", CharTriple{'a', 'b', 'c'}, 1},
-		{"abcde", CharTriple{'b', 'c', 'd'}, 1},
-		{"abcde", CharTriple{'c', 'd', 'e'}, 1},
-		{"abcde", CharTriple{'c', 'd', 'e'}, 1},
-		{"abcde 12 abcde", CharTriple{'c', 'd', 'e'}, 2},
+		{"ab", nil},
+		{"abc", []string{"abc"}},
+		{"abcd", []string{"abc", "bcd"}},
+		{"für", []string{"für"}},
+		{"Bäume", []string{"Bäu", "äum", "ume"}},
+		{"Größe", []string{"Grö", "röß", "öße"}},
 	}
 	for _, tc := range tests {
-		t.Run(tc.tokens, func(t *testing.T) {
-			m, _ := ReadCharTripleMap(sst{tc.tokens}, Word)
-			if got := m[tc.ts]; got != tc.want {
-				t.Fatalf("expected %d; got %d", tc.want, got)
+		t.Run(tc.test, func(t *testing.T) {
+			var got []string
+			EachChar3Gram(tc.test, func(str string) {
+				got = append(got, str)
+			})
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Fatalf("expected %v; got %v", tc.want, got)
 			}
 		})
 	}
 }
 
-func TestCharTripleMapAdd(t *testing.T) {
+func TestChar3GramMapSizes(t *testing.T) {
 	tests := []struct {
-		first, second string
-		ts            CharTriple
-		want          uint64
+		test       string
+		total, len uint64
 	}{
-		{"abc", "abc", CharTriple{'a', 'b', 'c'}, 2},
+		{"abababa", 5, 2},
+		{"abcde", 3, 3},
+	}
+	for _, tc := range tests {
+		t.Run(tc.test, func(t *testing.T) {
+			m := NewChar3Grams()
+			m.AddAll(tc.test)
+			if got := m.Len(); got != tc.len {
+				t.Fatalf("expected %d; got %d", tc.len, got)
+			}
+			if got := m.Total(); got != tc.total {
+				t.Fatalf("expected %d; got %d", tc.total, got)
+			}
+		})
+	}
+}
+
+func TestChar3GramMapAdd(t *testing.T) {
+	tests := []struct {
+		first, second, test string
+		want, total         uint64
+	}{
+		{"abc", "abc", "abc", 2, 2},
+		{"abc", "abc", "ab", 0, 2},
 	}
 	for _, tc := range tests {
 		t.Run(fmt.Sprintf("%s/%s", tc.first, tc.second), func(t *testing.T) {
-			a, _ := ReadCharTripleMap(sst{tc.first}, Word)
-			b, _ := ReadCharTripleMap(sst{tc.second}, Word)
-			m := a.Add(b)
-			if got := m[tc.ts]; got != tc.want {
+			a := NewChar3Grams()
+			a.AddAll(tc.first)
+			b := NewChar3Grams()
+			b.AddAll(tc.second)
+			a.Add3Grams(b)
+			if got := a.Get(tc.test); got != tc.want {
+				t.Fatalf("expected %d; got %d", tc.want, got)
+			}
+			if got := a.Total(); got != tc.total {
 				t.Fatalf("expected %d; got %d", tc.want, got)
 			}
 		})
 	}
-}
-
-type sst struct {
-	tokens string
-}
-
-func (s sst) Err() error { return nil }
-
-func (s sst) Tokens() <-chan Token {
-	strs := strings.Split(s.tokens, " ")
-	chn := make(chan Token, len(strs))
-	go func() {
-		defer close(chn)
-		for _, str := range strs {
-			chn <- Token{Str: str}
-		}
-	}()
-	return chn
 }
